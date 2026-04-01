c37f7b741a0aa69af602b8ad6f3d84d209583ad1
asus <zen>
2026-04-01 21:08:17 +0800

    ``` test(core): 添加PDF转图片功能的单元测试
    - 新增TestPdf2Img函数用于测试PDF转图片功能
    - 添加了测试用例路径配置
    - 补充相关注释说明测试命令
    ```

---
+++ core/p2i.go
@@ -0,0 +1,66 @@
+package core
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/zhangyiming748/finder"
+)
+
+func Pdf2Img(root string) {
+	folders := finder.FindAllFolders(root)
+	for _, folder := range folders {
+		if hasPdfInFolder(folder) {
+			Pdf2ImgInFolder(folder)
+		}
+	}
+}
+
+func hasPdfInFolder(root string) bool {
+	files := finder.FindAllFiles(root)
+	for _, file := range files {
+		if strings.HasSuffix(file, ".pdf") {
+			return true
+		}
+	}
+	return false
+
+}
+func Pdf2ImgInFolder(pdf string) {
+	files := finder.FindAllFiles(pdf)
+	var pdfFile string
+	for _, file := range files {
+		if strings.HasSuffix(file, ".pdf") {
+			pdfFile = file
+			break
+		}
+	}
+
+	if pdfFile == "" {
+		log.Println("未找到 PDF 文件!")
+		return
+	}
+
+	log.Printf("开始转换 PDF 文件:%v\n", pdfFile)
+
+	// 使用 ImageMagick 将 PDF 转换为图片
+	// magick -density 600 input.pdf output.png
+	outputPattern := filepath.Join(filepath.Dir(pdfFile), "Scan_%d.png")
+	args := []string{}
+	args = append(args, "-density", "300")
+	args = append(args, pdfFile)
+	args = append(args, outputPattern)
+	cmd := exec.Command("magick", args...)
+	log.Printf("执行命令:%v\n", cmd.String())
+
+	b, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("转换失败:", string(b))
+		log.Fatal(err)
+	}
+
+	log.Printf("转换完成，结果:%v\n", string(b))
+}
